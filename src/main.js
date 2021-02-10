4db73f0c60a41f894e678995ff1dd25a06f827f9
sarah <sarahaleesia1@gmail.com>
2021-02-10 01:34:22 -0600

    adds index and create functions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Add New Movie</h1>
+    Title: <input type="text" v-model="newTitle">
+    <br>
+    Director: <input type="text" v-model="newDirector">
+    <br>
+    Plot: <input type="text" v-model="newPlot">
+    <br>
+    Year: <input type="text" v-model="newYear">
+    <button v-on:click="createMovie()">Create</button>
+    <h1>All Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h3>{{ movie.title }}</h3>
+      <p>Director: {{ movie.director }}</p>
+      <p>Year: {{ movie.year }}</p>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      movies: [],
+      newTitle: "",
+      newDirector: "",
+      newYear: "",
+      newPlot: "",
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios
+        .get("/api/movies")
+        .then((response) => {
+          console.log(response.data);
+          this.movies = response.data;
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    createMovie: function () {
+      var params = {
+        title: this.newTitle,
+        director: this.newDirector,
+        plot: this.newPlot,
+        year: this.newYear,
+      };
+      axios
+        .post("http://localhost:3000/api/movies", params)
+        .then((response) => {
+          console.log(response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+  },
 };
 </script>
